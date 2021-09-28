2aecc605a049d16506745ba0446e408d5f85fa6e
Venkatesh Gunda <venkateshgunda1997@gmail.com>
2021-09-27 22:00:13 -0700

    Add Bootstrap Packages, CTokenGen Controller File

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
+const appIndex = require('./controllers/app.controller');
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {});
@@ -17,9 +18,14 @@ connection.once("open", () => {
 
 app.use(express.static(`${__dirname}/../client/build`));
 app.get("/health", (req,res) => {
-    res.send({
-        health: "UP"
-    });
+    // res.send({
+    //     health: "UP"
+    // });
+    res.send(appIndex.getIndexPage());
+});
+// Crypto Generation Route
+app.get("/tokengenerator", (req,res) => {
+    res.send(appIndex.getIndexPage());
 });
 
 app.get("/*", function(req,res) {
