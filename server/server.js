f327c7b8e43cb5158d581304f47efaab0e407e71
Venkatesh Gunda <venkateshgunda1997@gmail.com>
2021-09-27 22:31:44 -0700

    Add Token Generator and Token Validator APIs and Routes

--- server/server.js
+++ server/server.js
@@ -6,36 +6,19 @@ const app = express();
 const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
-const appIndex = require('./controllers/app.controller');
 
+// mongodb connection creation and opening
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {});
-
 const connection = mongoose.connection;
 connection.once("open", () => {
     console.log("MongoDB connection established successfully");
 });
 
-app.use(express.static(`${__dirname}/../client/build`));
-app.get("/health", (req,res) => {
-    // res.send({
-    //     health: "UP"
-    // });
-    res.send(appIndex.getIndexPage());
-});
-// Crypto Generation Route
-app.get("/tokengenerator", (req,res) => {
-    res.send(appIndex.getIndexPage());
-});
+app.use(express.static(`${__dirname}/../client/build`)); // re-direct client side routes to react
+require('./routes/routes')(app); // routes have been moved to routes.js file
 
-app.get("/*", function(req,res) {
-    res.sendFile(`${__dirname}/../client/build/index.html`, function(err) {
-        if(err) {
-            res.status(500).send(err);
-        }
-    });
-});
-
+// start the MERN app
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     console.log(`Click here to open: http://localhost:${port}`);
