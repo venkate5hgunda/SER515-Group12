7e507ac9280457e0c604cbb5ae4887c5a99623d6
Vishnu Preetham Reddy Dasari <vishnupreethamreddy@gmail.com>
2021-09-30 15:37:53 -0700

    Homepage -> About Us page
    Added information to about us button

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter as Router, Switch, Route}  from 'react-router-dom';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
 import Footer from './components/Footer/Footer'
 
 // TODO: Auto Build React when Node Starts
@@ -13,6 +14,7 @@ function App() {
         <Navbar></Navbar>
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/about' exact component={About}/>
         </Switch>
         <Footer />
       </Router>
---
+++ client/src/pages/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css';
+import AboutSection from '../../components/AboutSection/AboutSection';
+
+function About() {
+ return (
+  <div>
+   <AboutSection />
+  </div>
+ )
+}
+
+export default About
