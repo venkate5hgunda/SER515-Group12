6b13ab742cd8115c25ca95b1296c9378871e053f
Venkatesh Gunda <venkateshgunda1997@gmail.com>
2021-10-01 12:14:47 -0700

    Read/Verify Operation for Page Access

--- server/controllers/page-access-for-roles.controller.js
+++ server/controllers/page-access-for-roles.controller.js
@@ -1,5 +1,22 @@
 const pageAccessForRoles = require('../models/page-access-for-roles-model');
 
+async function verifyPageAccessInfo(pageInfo) {
+    try {
+        let response = await pageAccessForRoles.find({pageName: pageInfo.pageName});
+        if(!response || response.length==0) {
+            return false;
+        }
+        else {
+            console.log(response);
+            if(response[0].rolesWithAccess.indexOf(pageInfo.role)>-1) return true;
+            else return false;
+        }
+    }
+    catch(error) {
+        return error;
+    }
+}
+
 // pageInfo: { pageName: string, rolesWithAccess: string[] }
 // INSERT/UPDATE PAGE ACCESS INFORMATION
 async function upsertPageAccessInfo(pageInfo) {
@@ -49,6 +66,7 @@ async function removePageAccessInfo(pageInfo) {
 }
 
 module.exports = {
+    verifyPageAccessInfo: verifyPageAccessInfo,
     upsertPageAccessInfo: upsertPageAccessInfo,
     removePageAccessInfo: removePageAccessInfo
 };
