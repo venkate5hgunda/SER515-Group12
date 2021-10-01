a1a489f2862b3bf2ccc0c8eaf311b3bd5a651f49
Apple <apple@Apples-MacBook-Air-2.local>
2021-10-01 16:25:41 -0700

    Changes as per the review

--- server/controllers/base-schedule.controller.js
+++ server/controllers/base-schedule.controller.js
@@ -2,7 +2,20 @@ const baseScheduleService= require('../services/base-schedule.service')
 module.exports = {
     baseSchedule: () => {
     //Validation Request 
-    return baseScheduleService.baseSchedule()   
+
+    team=baseScheduleService.baseSchedule()
+
+    let i, j;
+    const pairs = [];
+
+    for(i=0; i < team.length; i++) {
+        for(j=0; j < team.length; j++) {
+            if(i!=j) {
+                pairs.push([team[i], team[j]])
+            }
+        }
+    }
+    return pairs;  
   
     }
 }
--- server/services/base-schedule.service.js
+++ server/services/base-schedule.service.js
@@ -1,16 +1,7 @@
 module.exports = {
     baseSchedule: () => {    
-    const team = ['A', 'B', 'C', 'D','E'];
-    let i, j;
-    const pairs = [];
-
-    for(i=0; i < team.length; i++) {
-        for(j=0; j < team.length; j++) {
-            if(i!=j) {
-                pairs.push([team[i], team[j]])
-            }
-        }
-    }
-        return pairs;
+    const team = ['A', 'B', 'C', 'D','E','F'];
+    
+    return team;
     }
 }
