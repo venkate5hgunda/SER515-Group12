64360e7a64f6a0d82848eb526720bb7b46929438
konark-bhad <53810525+konark-bhad@users.noreply.github.com>
2021-10-16 13:39:34 -0700

    story-44-task-65
    story-44-task-65-adding hotels section in Taskbar and creating a page for the same

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter as Router, Switch, Route}  from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
-import Faqs from './pages/Faqs/Faqs';
 import Footer from './components/Footer/Footer'
-import Hotels from './pages/Hotels/hotels';
 
 // TODO: Auto Build React when Node Starts
 function App() {
@@ -17,8 +15,6 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/about' exact component={About}/>
-          <Route path='/Faqs' exact component={Faqs}/>
-          <Route path='/hotels' exact component={Hotels}/>
         </Switch>
         <Footer />
       </Router>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import { Button } from '../Button/Button';
 import './Navbar.css'
-// import {FaAdobe} from "react-icons/fa";
+import {FaAdobe} from "react-icons/fa";
 
 function Navbar() {
  const [click, setClick] = useState(false);
@@ -26,7 +26,7 @@ function Navbar() {
      <div className='navbar-container'>
       
       <Link to="/" className="navbar-logo">
-      STW{/*  <FaAdobe color='red' size='3rem'/> */}
+       STW <FaAdobe color='red' size='3rem'/>
       </Link>
       <div className="menu-icon" onClick={handleClick}>
        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
@@ -41,16 +41,11 @@ function Navbar() {
        <li className='nav-item'>
         <Link to='/teams' className='nav-links' onClick={closeMobileMenu}>Teams</Link>
        </li>
+       <li className='nav-item'>
+        <Link to='/schedule' className='nav-links' onClick={closeMobileMenu}>Schedule</Link>
+       </li>
         <li className='nav-item'>
         <Link to='/players' className='nav-links' onClick={closeMobileMenu}>Players</Link>
-       
-       </li>
-       <li className='nav-item'>
-        <Link to='/faqs' className='nav-links' onClick={closeMobileMenu}>FAQs</Link>
-       
-       </li>
-       <li className='nav-item'>
-        <Link to='/hotels' className='nav-links' onClick={closeMobileMenu}>Hotels</Link>
        
        </li>
       </ul>
