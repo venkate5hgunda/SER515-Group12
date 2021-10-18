c21aa9da6e64931cf0c748b8cbe2ff01083d9125
konark-bhad <53810525+konark-bhad@users.noreply.github.com>
2021-10-18 07:55:00 -0700

    Story 44
    As a user, I need to see the detailed home page, so that I can info regarding the tournament

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter as Router, Switch, Route}  from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Footer from './components/Footer/Footer'
@@ -15,13 +15,13 @@ function App() {
       <Router>
         <Navbar></Navbar>
         <Switch>
-          <Route path='/' exact component={Home} />   
+          <Route path='/' exact component={Home} />
           <Route path='/about' exact component={About} />
           <Route path='/schedule' exact component={Schedule} />
         </Switch>
         <Footer />
       </Router>
-    </> 
+    </>
   );
 }
 
