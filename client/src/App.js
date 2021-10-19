e41b1f217bdaa5b31ca3cf60b5e41811ecf2077c
Venkatesh Gunda <venkateshgunda1997@gmail.com>
2021-10-18 22:58:16 -0700

    Story #74 :: Task #82 :: Commented Build Errors

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,9 @@ import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
-import Footer from './components/Footer/Footer'
-import Schedule from './pages/Schedule/Schedule'
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import Footer from './components/Footer/Footer';
+import Schedule from './pages/Schedule/Schedule';
+
 // TODO: Auto Build React when Node Starts
 function App() {
   return (
