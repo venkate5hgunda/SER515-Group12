d5282fe43e96411f90895d5d1914562e58b81676
Vishnu Preetham Reddy Dasari <vishnupreethamreddy@gmail.com>
2021-10-31 16:55:43 -0700

    Story71::Task90::Adding the images of the sponsors to make it more user friendly
    Added the logos of the companies who are sponsoring the tournament

--- client/src/pages/Sponsors/Sponsors.js
+++ client/src/pages/Sponsors/Sponsors.js
@@ -3,6 +3,13 @@ import '../../App.css';
 import { Container } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+import Adi from '../../../src/assets/images/adidas.png';
+import gat from '../../../src/assets/images/gatorade.png';
+import under from '../../../src/assets/images/new.png';
+import chips from '../../../src/assets/images/frito.png';
+import airline from '../../../src/assets/images/american.png';
+import network from '../../../src/assets/images/tsn.png';
 import './Sponsors.css';
 function Sponsors() {
  return (
@@ -14,34 +21,34 @@ function Sponsors() {
   <Container>
    <Row>
     <Col xs={4} md={4}>
-      
+       <Image className='image'src={Adi} rounded />
        <p className='text'>Adidas</p>
       
     </Col>
     <Col xs={4} md={4}>
-      
+       <Image className='image'src={gat} rounded />
        <p className='text'>The Gatorade Company</p>
       
     </Col>
     <Col xs={4} md={4}>
-      
+       <Image className='image'src={under} rounded />
        <p className='text'>Under Armour</p>
       
     </Col>
    </Row>
    <Row>
     <Col xs={4} md={4}>
-      
+       <Image className='image'src={chips} rounded />
        <p className='text'>Frito-Lays</p>
      
     </Col>
     <Col xs={4} md={4}>
-      
+       <Image className='image'src={airline} rounded /> 
        <p className='text'>American Airlines</p>
       
     </Col>
     <Col xs={4} md={4}>
-      
+       <Image className='image'src={network} rounded />
        <p className='text'>TSN Network</p>
       
     </Col>
