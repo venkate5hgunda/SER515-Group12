80cb581f675f5f129b21fd3e399d9ce82261b9e3
konark-bhad <53810525+konark-bhad@users.noreply.github.com>
2021-10-31 16:24:53 -0700

    story-72-task-94 Create google-map-react component to display the location of the tournament

---
+++ client/src/pages/Team/team.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import TeamSection from '../../components/TeamSection/TeamSection'
+
+function Teams() {
+ return (
+  <>
+   <TeamSection />
+  </>
+ );
+}
+
+export default Teams;
