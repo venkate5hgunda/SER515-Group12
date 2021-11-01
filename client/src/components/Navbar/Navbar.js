87a1fa43bcba7169129859491cf05b19b438b5d5
Venkatesh Gunda <venkateshgunda1997@gmail.com>
2021-10-31 20:04:04 -0700

    Story #69 :: Task #98 :: Fix Build Errors

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom';
 import { Button } from '../Button/Button';
 import './Navbar.css'
-import { FaAdobe } from "react-icons/fa";
 import {GiSoccerBall} from "react-icons/gi";
 import {
   ButtonGroup,
@@ -10,6 +9,7 @@ import {
   Container,
   Nav
 } from 'react-bootstrap';
+// import { ButtonGroup } from 'react-bootstrap';
 import TokenGenerator from '../../components/token-generator/token-generator';
 import { IconContext } from 'react-icons/lib';
 
@@ -92,37 +92,44 @@ function ReactNavbar() {
 
 export default Navbar
 
-            /* <Link to="/" className="navbar-logo">
-                        STW
-                        {<FaAdobe color='red' size='3rem'/>}
-                      </Link>
-                      <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-                      </div>
-                      <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                          <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
-                        </li>
-                        <li className='nav-item'>
-                          <Link to='/about' className='nav-links' onClick={closeMobileMenu}>About Us</Link>
-                        </li>
-                        <li className='nav-item'>
-                          <Link to='/teams' className='nav-links' onClick={closeMobileMenu}>Teams</Link>
-                        </li>
-                        <li className='nav-item'>
-                          <Link to='/schedule' className='nav-links' onClick={closeMobileMenu}>Schedule</Link>
-                        </li>
-                        <li className='nav-item'>
-                           <Link to='/hotels' className='nav-links' onClick={closeMobileMenu}>Hotels</Link>
-                        </li>
-                     <li className='nav-item'>
-                    <Link to='/sponsers' className='nav-links' onClick={closeMobileMenu}>Sponsers</Link>
-                   </li>
-                   <li className='nav-item'>
-                    <Link to='/Rules and Regulations' className='nav-links' onClick={closeMobileMenu}>Rules and Regulations</Link>
-                   </li>
-                   <li className='nav-item'>
-                    <Link to='/FAQ' className='nav-links' onClick={closeMobileMenu}>FAQ</Link>
-                   </li>
-                      </ul>
-                      <ButtonGroup className="me-2" aria-label="Invite Generation Button Group"> */
+//           <Link to="/" className="navbar-logo">
+//             STW
+//             {<SiAdobe color='red' size='3rem'/>}
+//           </Link>
+//           <div className="menu-icon" onClick={handleClick}>
+//             <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+//           </div>
+//           <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+//             <li className='nav-item'>
+//               <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
+//             </li>
+//             <li className='nav-item'>
+//               <Link to='/about' className='nav-links' onClick={closeMobileMenu}>About Us</Link>
+//             </li>
+//             <li className='nav-item'>
+//               <Link to='/teams' className='nav-links' onClick={closeMobileMenu}>Teams</Link>
+//             </li>
+//             <li className='nav-item'>
+//               <Link to='/schedule' className='nav-links' onClick={closeMobileMenu}>Schedule</Link>
+//             </li>
+//             <li className='nav-item'>
+//                <Link to='/hotels' className='nav-links' onClick={closeMobileMenu}>Hotels</Link>
+//             </li>
+//          <li className='nav-item'>
+//         <Link to='/sponsers' className='nav-links' onClick={closeMobileMenu}>Sponsers</Link>
+//        </li>
+//        <li className='nav-item'>
+//         <Link to='/Rules and Regulations' className='nav-links' onClick={closeMobileMenu}>Rules and Regulations</Link>
+//        </li>
+//        <li className='nav-item'>
+//         <Link to='/FAQ' className='nav-links' onClick={closeMobileMenu}>FAQ</Link>
+//        </li>
+//           </ul>
+//           <ButtonGroup className="me-2" aria-label="Invite Generation Button Group">
+//             <TokenGenerator />
+//           </ButtonGroup>
+//           {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+//         </div>
+//       </nav>
+//     </>
+// >>>>>>> Stashed changes
