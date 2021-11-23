112623d6becfa61ec2d4d95fcdd42a4a62826308
Venkatesh Gunda <venkateshgunda1997@gmail.com>
2021-11-22 19:00:00 -0700

    Story #149 :: Task #150 :: Fix Main Build Errors and UI Modifications

--- client/src/pages/Rules/Selection1.js
+++ client/src/pages/Rules/Selection1.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import '../../App.css';
-
+import { Card } from 'react-bootstrap';
 
 function Selection1() {
  return (
-  <div>
-   <p><b>Does the Classic have 2 or 3 divisions per age group?</b></p>
-   <p>Depending on the number of teams in any given age group applying to the adidas Warrior Soccer Classic, some age groups may ONLY consist of a Gold, Red White, etc. divisions with a White division if necessary. The Gold Division will be the upper division, Red is the middle division and White is the lower division. However, not all age groups will have 2 or more levels of play.</p>
-   <p><b>Where and when will the schedules be posted?</b></p>
-   <p>The schedule may be posted as early as the the first week in May. If you are listed as the contact for the team, you will be sent an email when the schedules are posted. Anyone can access the game schedules, scores and photos by clicking on the GAMES/SCORES tab in the MENU button across the top of any page OR by selecting your division in the drop down menu along the left side of any page on the site. You may also search by Club.</p>
-   
-  </div>
+  <Card className="rar-tab-content">
+      <Card.Body>
+          <p><b>Does the Classic have 2 or 3 divisions per age group?</b></p>
+          <p>Depending on the number of teams in any given age group applying to the adidas Warrior Soccer Classic, some age groups may ONLY consist of a Gold, Red White, etc. divisions with a White division if necessary. The Gold Division will be the upper division, Red is the middle division and White is the lower division. However, not all age groups will have 2 or more levels of play.</p>
+          <p><b>Where and when will the schedules be posted?</b></p>
+          <p>The schedule may be posted as early as the the first week in May. If you are listed as the contact for the team, you will be sent an email when the schedules are posted. Anyone can access the game schedules, scores and photos by clicking on the GAMES/SCORES tab in the MENU button across the top of any page OR by selecting your division in the drop down menu along the left side of any page on the site. You may also search by Club.</p>
+      </Card.Body>
+  </Card>
  )
 }
 
